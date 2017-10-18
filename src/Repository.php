4f8413ec0720f2fc098250d45b77f481532e5a4c
Johan Liljegren <johan.liljegren@prisjakt.nu>
2017-10-18 14:14:09 +0200

    Add first implementation of repository

--- src/Repository.php
+++ src/Repository.php
@@ -2,12 +2,161 @@
 
 namespace Prisjakt\Unleash;
 
-// Holds features.
-// Holds storage
-// Fetches updates
-// takes care of stale/expired data and locks.
-// config needed: http client, storage object, appName, instanceId
+use GuzzleHttp\Psr7\Request;
+use Http\Client\HttpClient;
+use Prisjakt\Unleash\Feature\Feature;
+use Prisjakt\Unleash\Helpers\Json;
+use Psr\Cache\CacheItemPoolInterface;
+
 class Repository
 {
+    const ENDPOINT_FEATURES = "/api/features";
+    const ENDPOINT_REGISTER = "/api/client/register";
 
+    private $unleashHost;
+    private $appName;
+    private $instanceId;
+    private $httpClient;
+    private $storage;
+
+    private $useCache;
+    private $cachePool;
+    private $updateInterval;
+    private $updateLockKey;
+
+    // TODO: move scalars to settings object?
+    public function __construct(
+        string $unleashHost,
+        string $appName,
+        string $instanceId,
+        HttpClient $httpClient,
+        Storage $storage,
+        int $updateInterval = 15,
+        CacheItemPoolInterface $cachePool = null
+    ) {
+        $this->appName = $appName;
+        $this->instanceId = $instanceId;
+        $this->httpClient = $httpClient;
+        $this->storage = $storage;
+
+        if (is_null($cachePool)) {
+            $this->useCache = false;
+        } else {
+            $this->useCache = true;
+            $this->cachePool = $cachePool;
+        }
+        $this->unleashHost = $unleashHost;
+        $this->updateInterval = $updateInterval;
+        $this->updateLockKey = "UPDATE_LOCK_{$this->appName}";
+    }
+
+    public function fetch()
+    {
+        if ($this->isStorageFresh()) {
+            return;
+        }
+
+        if ($this->isUpdateLocked()) {
+            return;
+        }
+
+        $this->lockUpdates();
+
+        $eTag = $this->storage->getETag() ?? "";
+        $request = new Request(
+            "get",
+            $this->unleashHost . self::ENDPOINT_FEATURES,
+            [
+                "If-None-Match" => $eTag,
+                "Content-Type" => "Application/Json",
+                "UNLEASH-APPNAME" => $this->appName,
+                "UNLEASH-INSTANCEID" => $this->instanceId,
+            ]
+        );
+
+        try {
+            $response = $this->httpClient->sendRequest($request);
+            $statusCode = $response->getStatusCode();
+            $eTag = implode(", ", $response->getHeader("Etag"));
+        } catch (\Exception $e) {
+            $this->ignoreOrFail();
+            return;
+        }
+
+        if ($statusCode === 304) {
+            if ($this->storage->getETag() === $eTag) {
+                $this->storage->resetLastUpdated();
+                return;
+            }
+        }
+
+        if ($statusCode < 200 || $statusCode > 299) {
+            $this->ignoreOrFail();
+            return;
+        }
+
+        $data = Json::decode($response->getBody()->getContents(), true);
+        $this->storage->reset($data["features"], $eTag);
+        $this->releaseUpdateLock();
+    }
+
+    public function has(string $key): bool
+    {
+        return $this->storage->has($key);
+    }
+
+    public function get(string $key): Feature
+    {
+        return $this->storage->get($key);
+    }
+
+    private function isUpdateLocked(): bool
+    {
+        if (!$this->useCache) {
+            return false;
+        }
+
+        $lockItem = $this->cachePool->getItem($this->updateLockKey);
+        if (!$lockItem->isHit()) {
+            return false;
+        }
+
+        return $lockItem->get() === true;
+    }
+
+    private function lockUpdates()
+    {
+        if (!$this->useCache) {
+            return;
+        }
+
+        $lockItem = $this->cachePool->getItem($this->updateLockKey);
+
+        $lockItem->set(true);
+        $this->cachePool->save($lockItem);
+    }
+
+    private function releaseUpdateLock()
+    {
+        if (!$this->useCache) {
+            return;
+        }
+
+        if ($this->cachePool->hasItem($this->updateLockKey)) {
+            $this->cachePool->deleteItem($this->updateLockKey);
+        }
+    }
+
+    private function isStorageFresh(): bool
+    {
+        return $this->storage->getLastUpdated() > (time() - $this->updateInterval);
+    }
+
+    private function ignoreOrFail()
+    {
+        if ($this->storage->hasData()) {
+            return;
+        }
+        throw new \Exception("Could not load data from anywhere (server, backup (or cache))");
+    }
 }
---
+++ tests/Feature/RepositoryTest.php
@@ -0,0 +1,281 @@
+<?php
+
+namespace Prisjakt\Unleash\Tests;
+
+use Cache\Adapter\PHPArray\ArrayCachePool;
+use GuzzleHttp\Psr7\Response;
+use Http\Mock\Client;
+use League\Flysystem\Adapter\NullAdapter;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Memory\MemoryAdapter;
+use PHPUnit\Framework\TestCase;
+use Prisjakt\Unleash\Feature\Feature;
+use Prisjakt\Unleash\Helpers\Json;
+use Prisjakt\Unleash\Repository;
+use Prisjakt\Unleash\Storage;
+
+// TODO: should probably move the first repository in two-stage tests to a helper function.
+class RepositoryTest extends TestCase
+{
+    private $unleashHost = "dummy-host:4242";
+    private $appName = "Test";
+    private $instanceId = "Test:Id";
+
+    public function testFetchDataFromServer()
+    {
+        $featuresData = $this->getFeaturesData();
+
+        $httpClient = new Client();
+        $httpClient->addResponse(new Response(200, [], Json::encode($featuresData)));
+
+        $storage = new Storage($this->appName, new Filesystem(new NullAdapter()));
+
+        $repository = new Repository($this->unleashHost, $this->appName, $this->instanceId, $httpClient, $storage);
+
+        $repository->fetch();
+
+        $this->assertTrue($repository->has("feature1"));
+        $this->assertTrue($repository->has("feature2"));
+    }
+
+    public function testUseStorageDataIfFresh()
+    {
+        $featuresData = $this->getFeaturesData();
+
+        $httpClient = new Client();
+        $httpClient->addResponse(new Response(200, [], Json::encode($featuresData)));
+
+        $storage = new Storage($this->appName, new Filesystem(new NullAdapter()));
+
+        $repository = new Repository($this->unleashHost, $this->appName, $this->instanceId, $httpClient, $storage);
+
+        $repository->fetch();
+        // Second pass. We should use cache now instead of asking the server.
+        $repository->fetch();
+        $requests = $httpClient->getRequests();
+
+        $this->assertEquals(1, count($requests));
+    }
+
+    public function testFetchFromServerIfStorageIsStale()
+    {
+        $featuresData = $this->getFeaturesData();
+
+        $httpClient = new Client();
+        $httpClient->addResponse(new Response(200, [], Json::encode($featuresData)));
+        $httpClient->addResponse(new Response(200, [], Json::encode($featuresData)));
+
+        $storage = new Storage($this->appName, new Filesystem(new NullAdapter()));
+
+        $repository = new Repository($this->unleashHost, $this->appName, $this->instanceId, $httpClient, $storage, 0);
+
+        $repository->fetch();
+        // Second pass. Since updateInterval is zero (0) the storage should immediately be considered stale.
+        $repository->fetch();
+
+        $requests = $httpClient->getRequests();
+        $this->assertEquals(2, count($requests));
+    }
+
+    public function testUseStaleStorageIfServerDoesNotRespond()
+    {
+        $featuresData = $this->getFeaturesData();
+
+        $httpClient = new Client();
+        $httpClient->addResponse(new Response(200, [], Json::encode($featuresData)));
+
+        $memoryFilesystem = new Filesystem(new MemoryAdapter());
+
+        $repositoryFromServer = new Repository(
+            $this->unleashHost,
+            $this->appName,
+            $this->instanceId,
+            $httpClient,
+            new Storage($this->appName, $memoryFilesystem)
+        );
+        $repositoryFromServer->fetch();
+
+        $repositoryFromStorage = new Repository(
+            $this->unleashHost,
+            $this->appName,
+            $this->instanceId,
+            $httpClient,
+            new Storage($this->appName, $memoryFilesystem),
+            0
+        );
+        $httpClient->addException(new \Exception("Server is down or something..."));
+        $repositoryFromStorage->fetch();
+
+        $requests = $httpClient->getRequests();
+        // we should still have made a total of two requests but even if the last response fails
+        $this->assertEquals(2, count($requests));
+        // we should still have data from cache/backup in the new instance.
+        $this->assertTrue($repositoryFromStorage->has("feature1"));
+    }
+
+    public function testLockedUpdateUsesStaleStorage()
+    {
+        $featuresData = $this->getFeaturesData();
+
+        $httpClient = new Client();
+        $httpClient->addResponse(new Response(200, [], Json::encode($featuresData)));
+
+        $storage = new Storage($this->appName, new Filesystem(new MemoryAdapter()));
+
+        $repositoryFromServer = new Repository(
+            $this->unleashHost,
+            $this->appName,
+            $this->instanceId,
+            $httpClient,
+            $storage
+        );
+        $repositoryFromServer->fetch();
+
+        $lockKey = "UPDATE_LOCK_{$this->appName}";
+        $cachePool = new ArrayCachePool();
+        $lockItem = $cachePool->getItem($lockKey);
+        $lockItem->set(true);
+        $cachePool->save($lockItem);
+
+        $staleRepository = new Repository(
+            $this->unleashHost,
+            $this->appName,
+            $this->instanceId,
+            $httpClient,
+            $storage,
+            0,
+            $cachePool
+        );
+        $staleRepository->fetch();
+
+        $requests = $httpClient->getRequests();
+        // we should still have made a total of two requests but even if the last response fails
+        $this->assertEquals(1, count($requests));
+        // we should still have data from cache/backup in the new instance.
+        $this->assertTrue($staleRepository->has("feature1"));
+    }
+
+    public function testValidETagResetsLastUpdated()
+    {
+        $featuresData = $this->getFeaturesData();
+
+        $httpClient = new Client();
+        $httpClient->addResponse(new Response(
+            200,
+            ["ETag" => "And that's all she wrote!"],
+            Json::encode($featuresData)
+        ));
+        $httpClient->addResponse(new Response(304, ["ETag" => "And that's all she wrote!"]));
+
+        $storage = new Storage($this->appName, new Filesystem(new NullAdapter()));
+        $repository = new Repository($this->unleashHost, $this->appName, $this->instanceId, $httpClient, $storage, 0);
+
+        $repository->fetch();
+        $repository->fetch();
+
+        $this->assertTrue($repository->has("feature1"));
+        $this->assertTrue($repository->has("feature2"));
+    }
+
+    public function testPassingACacheEnabledLockingUpdatesWhichWillNotDoAnythingWonky()
+    {
+        $featuresData = $this->getFeaturesData();
+        $cachePool = new ArrayCachePool();
+        $httpClient = new Client();
+
+        $httpClient->addResponse(new Response(200, [], Json::encode($featuresData)));
+
+        $storage = new Storage($this->appName, new Filesystem(new NullAdapter()));
+
+        $repository = new Repository(
+            $this->unleashHost,
+            $this->appName,
+            $this->instanceId,
+            $httpClient,
+            $storage,
+            0,
+            $cachePool
+        );
+
+        $repository->fetch();
+        $this->assertTrue($repository->has("feature1"));
+        $this->assertInstanceOf(Feature::class, $repository->get("feature1"));
+    }
+
+    public function testFailingToLoadFeaturesFromEverywhereThrows()
+    {
+        $httpClient = new Client();
+        $httpClient->addResponse(new Response(500));
+
+        $storage = new Storage($this->appName, new Filesystem(new NullAdapter()));
+
+        $repository = new Repository(
+            $this->unleashHost,
+            $this->appName,
+            $this->instanceId,
+            $httpClient,
+            $storage
+        );
+
+        $this->expectException(\Exception::class);
+        $repository->fetch();
+    }
+
+    public function testWrongResponseCodeUsesStaleStorageData()
+    {
+        $featuresData = $this->getFeaturesData();
+
+        $httpClient = new Client();
+        $httpClient->addResponse(new Response(200, [], Json::encode($featuresData)));
+        $httpClient->addResponse(new Response(500, [], "Oh no! Something went wrong on the server"));
+        $memoryFilesystem = new Filesystem(new MemoryAdapter());
+
+        $repositoryFromServer = new Repository(
+            $this->unleashHost,
+            $this->appName,
+            $this->instanceId,
+            $httpClient,
+            new Storage($this->appName, $memoryFilesystem)
+        );
+        $repositoryFromServer->fetch();
+
+        $httpClient = new Client();
+        $httpClient->addResponse(new Response(500));
+
+
+        $staleRepository = new Repository(
+            $this->unleashHost,
+            $this->appName,
+            $this->instanceId,
+            $httpClient,
+            new Storage($this->appName, $memoryFilesystem),
+            0
+        );
+
+        $staleRepository->fetch();
+        $this->assertTrue($staleRepository->has("feature1"));
+    }
+
+    private function getFeaturesData(): array
+    {
+        return [
+            "version" => 1,
+            "features" => [
+                [
+                    "name" => "feature1",
+                    "description" => "description1",
+                    "enabled" => false,
+                    "strategies" => [["name" => "default", "parameters" => []]],
+                    "createdAt" => time(),
+                ],
+                [
+                    "name" => "feature2",
+                    "description" => "description2",
+                    "enabled" => true,
+                    "strategies" => [["name" => "default", "parameters" => []]],
+                    "createdAt" => time(),
+                ],
+            ],
+        ];
+    }
+}
