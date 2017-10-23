0b79e04cef79984c6cfa92f2aad21b2cf145443d
Johan Liljegren <johan.liljegren@prisjakt.nu>
2017-10-23 11:47:24 +0200

    Add implementation of Unleash main client
    This is the user facing code.

--- src/Unleash.php
+++ src/Unleash.php
@@ -2,13 +2,79 @@
 
 namespace Prisjakt\Unleash;
 
-// Holds Strategies
-// Holds Repository
-// entry point
-// processes isEnabled()
-// pass all config here
-// config needed: http client, filesystem (optional), logger (optional), cachePool (optional),
-//                 appName, instanceId, strategyImplementations (optional)
+use GuzzleHttp\Psr7\Request;
+use Http\Client\HttpClient;
+use League\Flysystem\FilesystemInterface;
+use Prisjakt\Unleash\Feature\Processor;
+use Prisjakt\Unleash\Helpers\Json;
+use Prisjakt\Unleash\Strategy;
+use Psr\Cache\CacheItemPoolInterface;
+
+// TODO: add (optional) logging? (e.g. if feature does not exist/strategy not implemented/cant connect to server)
 class Unleash
 {
+    const ENDPOINT_REGISTER = "/api/client/register";
+
+    private $settings;
+    private $featureProcessor;
+    private $repository;
+    private $httpClient;
+
+    public function __construct(
+        Settings $settings,
+        array $strategies,
+        HttpClient $httpClient,
+        FilesystemInterface $filesystem = null,
+        CacheItemPoolInterface $cacheItemPool = null
+    ) {
+        $this->settings = $settings;
+        $this->httpClient = $httpClient;
+
+        $strategyRepository = new Strategy\Repository($strategies);
+        $this->featureProcessor = new Processor($strategyRepository);
+
+        $this->repository = new Repository(
+            $settings,
+            $httpClient,
+            new Storage($settings->getAppName(), $filesystem, $cacheItemPool),
+            $cacheItemPool
+        );
+
+        $this->register($strategyRepository->getNames());
+    }
+
+    public function isEnabled(string $key, array $context = [], bool $default = false): bool
+    {
+        $this->repository->fetch();
+
+        if (!$this->repository->has($key)) {
+            return $default;
+        }
+
+        $feature = $this->repository->get($key);
+
+        return $this->featureProcessor->process($feature, $context, $default);
+    }
+
+    private function register(array $implementedStrategies)
+    {
+        $request = new Request(
+            "post",
+            $this->settings->getUnleashHost() . self::ENDPOINT_REGISTER,
+            ["Content-Type" => "Application/Json"],
+            Json::encode([
+                "appName" => $this->settings->getAppName(),
+                "instanceId" => $this->settings->getInstanceId(),
+                "strategies" => $implementedStrategies,
+                "started" => time(),
+                "interval" => $this->settings->getUpdateInterval(),
+            ])
+        );
+
+        try {
+            $this->httpClient->sendRequest($request);
+        } catch (\Exception $e) {
+            // TODO: We should really catch more specific exceptions but yeah.. every adapter throws different exceptions.
+        }
+    }
 }
---
+++ tests/UnleashTest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Prisjakt\Unleash\Tests;
+
+use GuzzleHttp\Psr7\Response;
+use Http\Mock\Client;
+use League\Flysystem\Adapter\NullAdapter;
+use League\Flysystem\Filesystem;
+use PHPUnit\Framework\TestCase;
+use Prisjakt\Unleash\Helpers\Json;
+use Prisjakt\Unleash\Settings;
+use Prisjakt\Unleash\Strategy\StrategyInterface;
+use Prisjakt\Unleash\Unleash;
+use Prophecy\Argument\Token\AnyValueToken;
+
+class UnleashTest extends TestCase
+{
+    public function testClientRegistersOnInstantiation()
+    {
+        $settings = new Settings("appName", "instanceId");
+        $strategies = [];
+        $httpClient = new Client();
+        $filesystem = new Filesystem(new NullAdapter());
+
+        new Unleash($settings, $strategies, $httpClient, $filesystem);
+
+        // TODO: probably a bit too basic. Check request headers,content too.
+        $this->assertEquals(1, count($httpClient->getRequests()));
+    }
+
+    public function testClientRegisterIgnoresNetworkError()
+    {
+        $settings = new Settings("appName", "instanceId");
+        $strategies = [];
+        $httpClient = new Client();
+        $filesystem = new Filesystem(new NullAdapter());
+
+        $httpClient->addException(new \Exception("Oh no.. Something went wrong! is the internet down?"));
+
+        new Unleash($settings, $strategies, $httpClient, $filesystem);
+
+        $this->assertEquals(1, count($httpClient->getRequests()));
+    }
+
+    public function testIsEnabledWorks()
+    {
+        $defaultStrategyImpl = $this->prophesize(StrategyInterface::class);
+        $defaultStrategyImpl->getName()->willReturn("default");
+        $defaultStrategyImpl->isEnabled(new AnyValueToken(), new AnyValueToken())->willReturn(true);
+
+        $settings = new Settings("appName", "instanceId");
+        $strategies = [$defaultStrategyImpl->reveal()];
+        $httpClient = new Client();
+        $filesystem = new Filesystem(new NullAdapter());
+        $featuresData = $this->getFeaturesData();
+
+        $httpClient->addResponse(new Response()); // register request response
+        $httpClient->addResponse(new Response(200, [], Json::encode($featuresData)));
+
+        $unleash = new Unleash($settings, $strategies, $httpClient, $filesystem);
+
+
+        // Banana should always return false since the feature is disabled
+        $this->assertFalse($unleash->isEnabled("banana", [], false));
+        $this->assertFalse($unleash->isEnabled("banana", [], true));
+
+        // mango should return false since it exists, is enabled but we haven't implemented its strategies.
+        $this->assertFalse($unleash->isEnabled("mango", [], false));
+        $this->assertFalse($unleash->isEnabled("mango", [], true));
+
+        // pear should return default since we don't know anything about the feature at all. (it does not exist)
+        $this->assertFalse($unleash->isEnabled("pear", [], false));
+        $this->assertTrue($unleash->isEnabled("pear", [], true));
+
+        // guava should return true since it's enabled and we have implemented its strategy (which always return true).
+        $this->assertTrue($unleash->isEnabled("guava", [], false));
+        $this->assertTrue($unleash->isEnabled("guava", [], true));
+    }
+
+    private function getFeaturesData()
+    {
+        return [
+            "version" => 1,
+            "features" => [
+                [
+                    "name" => "banana",
+                    "description" => "description1",
+                    "enabled" => false,
+                    "strategies" => [["name" => "default", "parameters" => []]],
+                    "createdAt" => time(),
+                ],
+                [
+                    "name" => "mango",
+                    "description" => "description2",
+                    "enabled" => true,
+                    "strategies" => [["name" => "super-rare-strategy", "parameters" => []]],
+                    "createdAt" => time(),
+                ],
+                [
+                    "name" => "guava",
+                    "description" => "description3",
+                    "enabled" => true,
+                    "strategies" => [["name" => "default", "parameters" => []]],
+                    "createdAt" => time(),
+                ],
+
+            ],
+        ];
+    }
+}
