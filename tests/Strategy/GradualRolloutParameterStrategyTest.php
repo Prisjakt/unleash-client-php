929589a7341962a9ec84c1417c14ddf907f1d01e
Johan Liljegren <johan.liljegren@prisjakt.nu>
2017-10-23 17:22:28 +0200

    Add Strategy implementation(s) for single-parameter-based rollout strategies

---
+++ tests/Strategy/GradualRolloutParameterStrategyTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Prisjakt\Unleash\Tests\Strategy;
+
+use lastguest\Murmur;
+use PHPUnit\Framework\TestCase;
+use Prisjakt\Unleash\Strategy\GradualRolloutParameterStrategy;
+
+class GradualRolloutParameterStrategyTest extends TestCase
+{
+    public function testShouldBeEnabledIfPercentageIsSameAsNormalizedHash()
+    {
+
+        $parameter = "sessionId";
+        $sessionId = "385h4389r2nr32423";
+        $hashKey = "{$sessionId}:groupId";
+        $parameters = [
+            "percentage" => Murmur::hash3_int($hashKey) % 100,
+            "groupId" => "groupId",
+        ];
+
+        $strategy = $this->getMockForAbstractClass(GradualRolloutParameterStrategy::class, [$parameter]);
+
+        $this->assertTrue($strategy->isEnabled($parameters, [$parameter => $sessionId]));
+    }
+
+    public function testZeroPercentageMeansDisabled()
+    {
+        $parameter = "sessionId";
+        $strategy = $this->getMockForAbstractClass(GradualRolloutParameterStrategy::class, [$parameter]);
+
+
+        $parameters = [
+            "percentage" => 0,
+            "groupId" => "groupId",
+        ];
+        $context = [
+            $parameter => "mango guava",
+        ];
+
+        $this->assertFalse($strategy->isEnabled($parameters, $context));
+    }
+
+    public function testOneHundredPercentMeansEnabled()
+    {
+        $parameter = "sessionId";
+        $strategy = $this->getMockForAbstractClass(GradualRolloutParameterStrategy::class, [$parameter]);
+
+        $parameters = [
+            "percentage" => 100,
+            "groupId" => "groupId",
+        ];
+        $context = [
+            $parameter => "sorry, eh",
+        ];
+
+        $this->assertTrue($strategy->isEnabled($parameters, $context));
+    }
+
+    public function testParameterNotInContextMeansDisabled()
+    {
+        $strategy = $this->getMockForAbstractClass(GradualRolloutParameterStrategy::class, ["unknownParameter"]);
+
+        $parameters = [
+            "percentage" => 100,
+            "groupId" => "groupId",
+        ];
+        $this->assertFalse($strategy->isEnabled($parameters, []));
+    }
+}
---
+++ tests/Strategy/GradualRolloutUserIdStrategyTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Prisjakt\Unleash\Tests\Strategy;
+
+use Prisjakt\Unleash\Strategy\GradualRolloutUserIdStrategy;
+use PHPUnit\Framework\TestCase;
+
+// Thin on tests here since most of the logic is tested in GradualRolloutParameterStrategyTest
+class GradualRolloutUserIdStrategyTest extends TestCase
+{
+    public function testStrategyReturnsCorrectName()
+    {
+        $strategy = new GradualRolloutUserIdStrategy();
+
+        $this->assertEquals("gradualRolloutUserId", $strategy->getName());
+    }
+
+    public function testStrategyReturnsTrueForOneHundredPercent()
+    {
+        $strategy =new GradualRolloutUserIdStrategy();
+
+        $parameters = [
+            "percentage" => 100,
+            "groupId" => "groupId",
+        ];
+        $context = [
+            "userId" => "sorry, eh",
+        ];
+
+        $this->assertTrue($strategy->isEnabled($parameters, $context));
+    }
+
+}
