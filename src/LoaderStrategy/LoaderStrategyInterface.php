e86fbb86e06d7ecb48b34b54999510526e37e125
Johan Liljegren <johan.liljegren@prisjakt.nu>
2018-01-09 14:16:26 +0100

    Add phpdoc to DataLoaderInterface

--- src/LoaderStrategy/LoaderStrategyInterface.php
+++ src/LoaderStrategy/LoaderStrategyInterface.php
@@ -3,8 +3,16 @@
 namespace Prisjakt\Unleash\LoaderStrategy;
 
 use Prisjakt\Unleash\Backend;
+use Prisjakt\Unleash\DataStorage;
 
 interface LoaderStrategyInterface
 {
+    /**
+     * Called with a backend, implement strategy to fetch data
+     * Can use supplied $backend for easy access to cache, file and server or you can roll your own
+     * just as long as you return an instance of DataStorage or null.
+     * @param Backend $backend
+     * @return DataStorage|null
+     */
     public function load(Backend $backend);
 }
